5f9a3848f697809f7d2033bf12d3c9d0d392f82a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-08-08 09:14:15 +0400

    add user controllers

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sqlite3'
 gem 'validates'
 gem 'haml'
 gem 'haml-rails'
+
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      user_sign_in user
+      redirect_to
+    else
+    end
+  end
+end
