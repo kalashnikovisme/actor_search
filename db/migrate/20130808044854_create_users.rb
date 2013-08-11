b4d1569225df36b1158ad0ffdf553e4b09dced9b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-08-12 03:24:24 +0400

    change migrations

--- db/migrate/20130808044854_create_users.rb
+++ db/migrate/20130808044854_create_users.rb
@@ -3,7 +3,11 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :login
       t.string :password
-
+      t.text :first_name
+      t.text :middle_name
+      t.text :last_name
+      t.text :phone
+      t.text :email
       t.timestamps
     end
   end
--- db/migrate/20130808170105_create_actors.rb
+++ db/migrate/20130808170105_create_actors.rb
@@ -1,11 +1,6 @@
 class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :first_name
-      t.string :middle_name
-      t.string :last_name
-      t.string :phone
-      t.string :email
       t.integer :user_id
 
       t.timestamps
