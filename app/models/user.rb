fd002a6d2a51972dd743fd5c7e7d9a3ba092057e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-08-12 03:21:08 +0400

    change models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :login, :password
+  attr_accessible :login,
+                  :password,
+                  :email,
+                  :first_name,
+                  :middle_name,
+                  :last_name,
+                  :phone
 
   has_one :actor, dependent: :destroy
   has_one :model, dependent: :destroy
@@ -8,4 +14,12 @@ class User < ActiveRecord::Base
                     length: { maximum: 8 }
   validates :password, presence: true,
                        length: { minumum: 8, maximum: 16 }
+
+  validates :email, presence: true,
+                    email: true,
+                    uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :middle_name, presence: true
+  validates :phone, presence: true
 end
